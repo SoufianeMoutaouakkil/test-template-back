150a06e85089cf8a1b7b86bc2403dbdcd8f44e28
SoufianeMoutaouakkil <soufiane.moutaouakkil.ma@gmail.com>
2024-02-10 10:42:15 +0100

    add privacy policy

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import FacebookCallback from "./components/auth/FacebookCallback";
+import Contact from "./pages/Contact";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/Login" element={<Login />} />
         <Route path="/auth/google/callback" element={<GoogleCallback />} />
         <Route path="/auth/facebook/callback" element={<FacebookCallback />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
       </Routes>
     </Router>
   );
