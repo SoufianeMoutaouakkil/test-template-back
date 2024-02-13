8cef774906039c8d39b9eea013547564ec0baad1
SoufianeMoutaouakkil <soufiane.moutaouakkil.ma@gmail.com>
2024-02-13 21:23:14 +0100

    Update HandlingFile.jsx

--- src/pages/HandlingFile.jsx
+++ src/pages/HandlingFile.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const HandlingFile = () => {
   const [file, setFile] = useState(null);
   const [username, setUsername] = useState("");
   const [filename, setFilename] = useState("");
+  const backBaseUrl = import.meta.env.VITE_BACK_URL;
+  const attachmentsUrl = backBaseUrl + "/api/v1/attachments";
 
   const handleFileChange = (event) => {
+    console.log("File changed : ", event.target.files[0]);
     setFile(event.target.files[0]);
   };
   const handleUsernameChange = (event) => {
@@ -16,12 +19,26 @@ const HandlingFile = () => {
   };
 
   const handleUpload = async () => {
+    console.log("Uploading file", file);
     if (!file) return;
     const formData = new FormData();
     formData.append("file", file);
     formData.append("filename", filename);
     formData.append("username", username);
-    const response = await fetch("http://localhost:5000/api/v1/attachments", {
+    const response = await fetch(attachmentsUrl, {
+      method: "POST",
+      body: formData,
+    });
+    const data = await response.json();
+    console.log(data);
+  };
+  const handleUploadFirebase = async () => {
+    if (!file) return;
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("filename", filename);
+    formData.append("username", username);
+    const response = await fetch(attachmentsUrl + "/upload-to-firebase", {
       method: "POST",
       body: formData,
     });
@@ -31,31 +48,53 @@ const HandlingFile = () => {
   const handleDownload = async () => {
     if (!filename) return;
     console.log("Downloading file", filename);
-    const response = await fetch(
-      "http://localhost:5000/api/v1/attachments/download",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          filename,
-          username,
-        }),
-      }
-    );
+    const response = await fetch(attachmentsUrl + "/download", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        filename,
+        username,
+      }),
+    });
     if (response.ok) {
-      const blob = await response.blob();
-      const url = window.URL.createObjectURL(blob);
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute('download', filename);
-      document.body.appendChild(link);
-      link.click();
-      link.remove();
+      await downloadFile(response);
     } else {
-      console.error('File download failed:', await response.text());
+      console.error("File download failed:", await response.text());
     }
+  };
+  const handleDownloadFirebase = async () => {
+    if (!filename) return;
+    console.log("Downloading file", filename);
+    const response = await fetch(attachmentsUrl + "/download-from-firebase", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        filename,
+        username,
+      }),
+    });
+    if (response.ok) {
+      //   const data = await response.json();
+      //   console.log(data);
+      await downloadFile(response);
+    } else {
+      console.error("File download failed:", await response.text());
+    }
+  };
+
+  const downloadFile = async (response) => {
+    const blob = await response.blob();
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.setAttribute("download", filename);
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
   };
 
   return (
@@ -74,6 +113,8 @@ const HandlingFile = () => {
       <br />
       <button onClick={handleDownload}>Download</button>
       <button onClick={handleUpload}>Upload</button>
+      <button onClick={handleUploadFirebase}>Upload Firebase</button>
+      <button onClick={handleDownloadFirebase}>Download Firebase</button>
       <button onClick={() => setFile(null)}>Clear</button>
     </div>
   );
